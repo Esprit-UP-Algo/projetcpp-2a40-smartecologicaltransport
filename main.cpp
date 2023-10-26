9ca0738528372b22ac509408f9e2f4c38545d80f
Talel <talel.benaziza@esprit.tn>
2023-10-26 12:11:07 +0100

    main crud

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include "mainwindow.h"
+
 #include <QApplication>
-#include <QMessageBox>
-#include "connection.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    Connection c;
-    bool test=c.createconnect();
-    if(test)
-    {w.show();
-        QMessageBox::critical(nullptr, QObject::tr("database is open"),
-                    QObject::tr("connection successful.\n"
-                                "Click Cancel to exit."), QMessageBox::Cancel);
+    inter f(&w,{0,0,1930,990},"background : qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:2, stop:0.2 #6ac963 , stop:0.5 #4aa66a, stop:0.7 #0e7285);border-radius:60px;","");
+    inter f2(&w,{160,50,1700,900},"background-color:white;border-radius:40px","");
+    inter f_crud(&w,{300,200,1500,700},"background-color:white;border-radius:40px",":/new/titre/image/titre/gestion de cours/Liste des courses.png");
+    btn_crud btn[5];
+    for(int i=0;i<5;i++)
+    {
+        btn[i]=btn_crud(f_crud.lab,{150+i*250,50,220,50},"",":/new/crud/image/boutton_crud/"+QString :: number(i)+".png");
+    }
+    btn[0].b->setText("   ajout");
+    btn[1].b->setText("   suprimer");
+    btn[2].b->setText("    mise ajours");
+    btn[3].b->setText("   filtrer");
+    btn[4].b->setText("   recherche");
 
-}
-    else
-        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
-                    QObject::tr("connection failed.\n"
-                                "Click Cancel to exit."), QMessageBox::Cancel);
-
-
-
+    w.show();
     return a.exec();
 }
