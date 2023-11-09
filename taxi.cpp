91f16b6e343ce593b0612ee845b8a858ad246cfb
Ssassi30 <slim.sassi@esprit.tn>
2023-11-09 12:30:59 +0100

    ajout taxi

--- taxi.cpp
+++ taxi.cpp
@@ -1,6 +1,21 @@
 #include "taxi.h"
 #include<QString>
-
+#include <QLineEdit>
+#include <QKeyEvent>
+#include <QApplication>
+#include <QLabel>
+#include <QTimer>
+#include <QPainter>
+#include <QPdfWriter>
+#include <QPrinter>
+#include <QPainter>
+#include <QFileDialog>
+#include <QTextDocument>
+#include <QTextCursor>
+#include <QTextDocumentWriter>
+#include <QFileDialog>
+#include <QtWidgets>
+#include <QPdfWriter>
 taxi :: taxi(QString immatricule ,QString marque ,QString modele ,QString carburant ,int NBRPL ,QString BoiteV ,QString Statut ,QString NumCH,QString TypeCO,int EtatCH ,QString Auton)
 {
     this->immatricule=immatricule;
@@ -67,6 +82,38 @@ QSqlQueryModel *taxi::Recherher(QString immatricule )
 
        return model;
 }
+QSqlQueryModel *taxi::Recherher0(QString immatricule )
+{
+    QSqlQueryModel *model = new QSqlQueryModel();
+       QSqlQuery query;
+       query.prepare("SELECT * FROM  HISTO WHERE  immatricule = :immatricule");
+       query.bindValue(":immatricule", immatricule);
+       query.exec();
+
+       model->setQuery(query);
+
+       model->setHeaderData(0, Qt::Horizontal, QObject::tr("Immatricule"));
+
+       return model;
+}
+int taxi::Recherher2(QString immatricule)
+{
+    QSqlQuery query;
+        query.prepare("SELECT COUNT(*) FROM TAXIS WHERE immatricule=:immatricule");
+        query.exec();
+                query.next();
+            int count = query.value(0).toInt();
+            return count;
+}
+int taxi::Recherher3(QString immatricule)
+{
+    QSqlQuery query;
+        query.prepare("SELECT COUNT(*) FROM HISTO WHERE immatricule=:immatricule");
+        query.exec();
+                query.next();
+            int count = query.value(0).toInt();
+            return count;
+}
 bool taxi :: update(QString immatricule ,QString marque, QString modele , QString carburant , int NBRPL , QString BoiteV , QString Statut , QString NumCH, QString TypeCO , int EtatCH , QString Auton)
 {
     QSqlQuery query;
@@ -111,6 +158,180 @@ int taxi :: GTO()
                 query.next();
             int count = query.value(0).toInt();
             return count;
+}
+//RECUPERATION
+QString taxi :: recupererMarque(QString immatricule )
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           QString marque = query.value("marque").toString();
+           return marque;
+}
+}
+QString taxi :: recupererModele(QString immatricule )
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           QString modele = query.value("modele").toString();
+           return modele;
+}
+}
+QString taxi :: recupererCarburant(QString immatricule)
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           QString carburant = query.value("carburant").toString();
+           return carburant;
+}
+}
+int taxi :: recupererNBRPL(QString immatricule)
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           int NBRPL = query.value("NBRPL").toInt();
+           return NBRPL;
+}
+}
+QString taxi :: recupererBoiteV(QString immatricule)
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           QString B = query.value("BoiteV").toString();
+           return B;
+}
+}
+QString taxi :: recupererStatut(QString immatricule)
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           QString B = query.value("Statut").toString();
+           return B;
+}
+}
+QString taxi :: recupererNUMCH(QString immatricule)
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           QString B = query.value("NUMCH").toString();
+           return B;
+}
+}
+QString taxi :: recupererTYEPCO(QString immatricule)
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           QString B = query.value("TypeCO").toString();
+           return B;
+}
+}
+int taxi :: recupererETATCH(QString immatricule)
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           int NBRPL = query.value("EtatCH").toInt();
+           return NBRPL;
+}
+}
+QString taxi :: recupererAUTON(QString immatricule)
+{
+    QSqlQuery query;
+       query.prepare("SELECT * FROM taxis WHERE immatricule=:immatricule");
+       query.bindValue(":immatricule", immatricule);
+
+       if (query.exec() && query.next()) {
+           QString B = query.value("Auton").toString();
+           return B;
+}
+}
+void taxi :: showLabelFor5Seconds(QLabel *label)
+{
+    label->show();
+
+    QTimer *timer = new QTimer();
+    timer->setSingleShot(true);
+    QObject::connect(timer, &QTimer::timeout, label, &QLabel::hide);
+    timer->start(5000); // Démarrer le timer pour masquer le QLabel après 5 secondes.
+}
+QSqlQueryModel *taxi::afficherHISTO()
+{
+    QSqlQueryModel *model= new QSqlQueryModel();
+    model->setQuery("select * from HISTO");
+    model->setHeaderData(0,Qt::Horizontal,QObject::tr("Immatricule"));
+    return model;
+}
+void taxi :: generatePdf(const QString &fileName)
+{
+    QSqlQuery query, query2;
+        query.prepare("SELECT COUNT(*) FROM TAXIS WHERE statut = 'occupe'");
+        query.exec();
+                query.next();
+            int count = query.value(0).toInt();
+            //Les taxis libres
+                query2.prepare("SELECT COUNT(*) FROM TAXIS WHERE statut = 'libre'");
+                query2.exec();
+                        query2.next();
+                    int count1 = query2.value(0).toInt();
+            QString myText = QString::number(count);
+            QString myText1 = QString::number(count1);
+    QPdfWriter pdfWriter(fileName);
+    QPainter painter(&pdfWriter);
+
+    // Set up the painter
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setRenderHint(QPainter::TextAntialiasing, true);
+    //New Page
+    painter.begin(&pdfWriter);
+    // Start a new page
+    QFont font;
+    int fontSize = 25;  // Adjust the font size as needed
+    font.setPointSize(fontSize);
+    font.setBold(true);
+    painter.setFont(font);
+    painter.setPen(Qt::black);
+    painter.begin(&pdfWriter);
+
+    // Ajouter une section d'en-tête
+    painter.drawText(500, 900, "Rapport Instantané sur l'Activité des Taxis");
+
+    painter.drawText(0,3800 , "Signature : Sassi Slim");
+    painter.drawText(0, 4200, "Date de Création : " + QDate::currentDate().toString("dd/MM/yyyy"));
+    painter.setPen(Qt::black);
+    painter.drawText(2300, 2300, "Entreprise : GreenGO");
+    painter.setPen(Qt::black); // Définir la couleur du stylo en noir
+    painter.drawLine(0, 1100, pdfWriter.width(),1100);
+
+    // Afficher les informations sur les taxis dans le PDF
+    painter.setPen(Qt::red);
+    painter.drawText(0, 2800, "Nombre de taxis occupés : " + myText);
+    painter.drawText(0, 3200, "Nombre de taxis libres : " + myText1);
+    painter.end();
 }
 taxi::taxi()
 {
