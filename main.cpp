a77f5915ff8cbb70f771857902111290a57bdbe8
Talel <talel.benaziza@esprit.tn>
2023-11-09 13:40:40 +0100

    lajout du main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,26 @@
 #include "mainwindow.h"
-
 #include <QApplication>
-
+#include <QMessageBox>
+#include "connection.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    inter f(&w,{0,0,1930,990},"background : qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:2, stop:0.2 #6ac963 , stop:0.5 #4aa66a, stop:0.7 #0e7285);border-radius:60px;","");
-    inter f2(&w,{160,50,1700,900},"background-color:white;border-radius:40px","");
-    inter f_crud(&w,{300,200,1500,700},"background-color:white;border-radius:40px",":/new/titre/image/titre/gestion de conducteurs/Liste des conducteurs.png");
-    btn_crud btn[5];
-    for(int i=0;i<5;i++)
-    {
-        btn[i]=btn_crud(f_crud.lab,{150+i*250,50,220,50},"",":/new/crud/image/boutton_crud/"+QString :: number(i)+".png");
-    }
-    btn[0].b->setText("   ajout");
-    btn[1].b->setText("   suprimer");
-    btn[2].b->setText("    mise ajours");
-    btn[3].b->setText("   filtrer");
-    btn[4].b->setText("   recherche");
+    Connection c;
+    bool test=c.createconnect();
+    if(test)
+    {w.show();
+        QMessageBox::critical(nullptr, QObject::tr("database is open"),
+                    QObject::tr("connection successful.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
 
-    w.show();
+}
+    else
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                    QObject::tr("connection failed.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
     return a.exec();
 }
