b1fb28961f43c126ed6056a1344bca368cf40b1e
Asma <Asma.messaoudi@esprit.tn>
2023-11-09 15:15:53 +0100

    ajout main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include <string>
-using namespace std;
-int main()
+#include "mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
+#include "connection.h"
+int main(int argc, char *argv[])
 {
-    cout<<"helllo world!"<<endl;
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    Connection c;
+    bool test=c.createconnect();
+    if(test)
+    {w.show();
+        QMessageBox::critical(nullptr, QObject::tr("database is open"),
+                    QObject::tr("connection successful.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+    else
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                    QObject::tr("connection failed.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+    return a.exec();
 }
